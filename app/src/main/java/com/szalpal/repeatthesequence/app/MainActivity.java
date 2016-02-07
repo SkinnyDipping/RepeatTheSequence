e7c8c7e1c2cf553c331588a2ebc3dbe550c299b2
SkinnyDipping <spam_szala@interia.pl>
2016-02-07 12:48:14 +0100

    Removed CompatActivity

--- app/src/main/java/com/szalpal/repeatthesequence/app/MainActivity.java
+++ app/src/main/java/com/szalpal/repeatthesequence/app/MainActivity.java
@@ -1,11 +1,11 @@
 package com.szalpal.repeatthesequence.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     public static final String TAG = "SzalGame";
 
